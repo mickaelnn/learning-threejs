adea5883fcf65c750e1a53935d797fd9d35c97e0
mickaelnn <mickael.naumann@gmail.com>
2021-09-25 11:21:58 -0300

    feat: Debuguer

--- base-debug-ui/src/script.js
+++ base-debug-ui/src/script.js
@@ -2,6 +2,34 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true, width: 400 })
+
+const parameters = {
+    color:  0xff0000,
+    spin: () => {
+        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2})
+    },
+    barrelRoll: () => {
+        gsap.to(mesh.rotation, { duration: 1, x: mesh.rotation.x + Math.PI * 1})
+    }
+}
+
+gui
+    .add(parameters, 'spin')
+
+    gui
+    .add(parameters, 'barrelRoll')
+
+gui
+    .addColor(parameters, 'color')
+    .onChange(() => {
+        material.color.set(parameters.color)
+    })
 
 /**
  * Base
@@ -16,9 +44,23 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Debug
+gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('Box Y')
+
+gui
+    .add(mesh, 'visible')
+
+gui
+    .add(material, 'wireframe')
 
 /**
  * Sizes
